5d7040de3437a791cbc4c6c72e6f2e386777ece1
dediirawan201 <dediirwansyah1925@gmail.com>
2022-10-31 21:02:26 +0700

    menambahkan efect sound

--- src/components/Soal.jsx
+++ src/components/Soal.jsx
@@ -1,10 +1,24 @@
 import { useEffect } from "react"
 import { useState } from "react"
+import useSound from "use-sound"
+import play from "../sounds/play.mp3"
+import correct from '../sounds/correct.mp3'
+import wait from '../sounds/wait.mp3'
+import wrong from '../sounds/wrong.mp3'
 
 const Soal = ({data,questionNumber,setQuestionNumber,setStop}) => {
     const [question, setQuestion] = useState(null)
     const [selectedAnswer,setSelectedAnswer] = useState(null)
     const [classNama,setClassName] = useState('jawaban')
+
+    const [letsPlay] = useSound(play)
+    const [correctAnswer] = useSound(correct)
+    const [wrongAnswer] = useSound(wrong)
+
+    useEffect(() => {
+        letsPlay();
+    },[letsPlay])
+
     useEffect(() => {
         setQuestion(data[questionNumber - 1])
     },[data,questionNumber])
@@ -18,12 +32,18 @@ const Soal = ({data,questionNumber,setQuestionNumber,setStop}) => {
         setSelectedAnswer(a)
         setClassName('jawaban active')
         delay(3000, () => setClassName(a.correct ? 'jawaban correct' : 'jawaban wrong'))
-        delay(6000, () => {
+        delay(5000, () => {
             if(a.correct){
-                setQuestionNumber((prev) => prev + 1)
-                setSelectedAnswer(null)
+                correctAnswer()
+                delay(1000, () => {
+                    setQuestionNumber((prev) => prev + 1)
+                    setSelectedAnswer(null)
+                })
             } else{
-                setStop(true)
+                wrongAnswer()
+                delay(1000, () => {
+                    setStop(true)
+                })
             }
         })
     }
@@ -31,6 +51,7 @@ const Soal = ({data,questionNumber,setQuestionNumber,setStop}) => {
     <div className="soal">
         <div className="pertanyaan">{question?.pertanyaan}</div>
         <div className="jawabans">
+            {/* <div onClick={() => letsPlay()}>cek</div> */}
             {question?.jawabans.map((a,indek) => (
                 <div key={indek} className={selectedAnswer === a ? classNama : 'jawaban'} onClick={() => handleClick(a)}>{a.text}</div>
             ))}
